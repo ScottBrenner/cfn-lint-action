7dbce16d761284724a599379a76b7b5d0a7305b3
softprops <d.tangren@gmail.com>
2019-12-24 23:52:01 -0500

    add matcher def to host system as docker actions won't resolve it

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
-echo "::add-matcher::/cfn-lint.json"
+# Copy the matcher to the host system; otherwise "add-matcher" can't find it.
+cp /cfn-lint.json /github/workflow/cfn-lint.json
+echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/cfn-lint.json"
 
 sh -c "cfn-lint $*"
