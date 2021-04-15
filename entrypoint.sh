cb7854543236ed277c903822e4eb1d374c0088bd
NickLiffen <nickliffen@gmail.com>
2021-04-15 17:33:56 +0100

    Pushing build

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# Copy the matcher to the host system; otherwise "add-matcher" can't find it.
-cp /cfn-lint.json /github/workflow/cfn-lint.json
-echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/cfn-lint.json"
-
-sh -c "cfn-lint $*"
