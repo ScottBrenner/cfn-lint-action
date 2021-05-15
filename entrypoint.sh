c8de4a647dafb4d2e6dbf20bc09d1e4cd959fd26
Scott Brenner <scott@scottbrenner.me>
2021-05-15 12:13:33 -0700

    Use Actions syntax for warning message

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "INFO: This version of cfn-lint-action is deprecated, please upgrade via https://github.com/ScottBrenner/cfn-lint-action#upgrading-from-version-1"
+echo "::warning ::This version of cfn-lint-action is deprecated, please upgrade via https://github.com/ScottBrenner/cfn-lint-action#upgrading-from-version-1"
 
 # Copy the matcher to the host system; otherwise "add-matcher" can't find it.
 cp /cfn-lint.json /github/workflow/cfn-lint.json
