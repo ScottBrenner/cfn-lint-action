6014d713d58a602d9c2373fa5301bc7427304055
Scott Brenner <scott@scottbrenner.me>
2026-04-17 05:07:02 +0000

    Rework install logic

--- tests/setup.test.js
+++ tests/setup.test.js
@@ -18,32 +18,65 @@ test.each([
   {
     platform: "linux",
     input: {},
-    expected: { version: "[full]==1.*", python: "python3" },
+    expected: {
+      version: "[full]==1.*",
+      python: "python3",
+      install: "cfn-lint[full]==1.*",
+    },
   },
   {
     platform: "darwin",
     input: {},
-    expected: { version: "[full]==1.*", python: "python3" },
+    expected: {
+      version: "[full]==1.*",
+      python: "python3",
+      install: "cfn-lint[full]==1.*",
+    },
   },
   {
     platform: "win32",
     input: {},
-    expected: { version: "[full]==1.*", python: "python" },
+    expected: {
+      version: "[full]==1.*",
+      python: "python",
+      install: "cfn-lint[full]==1.*",
+    },
   },
   {
     platform: "linux",
     input: { version: "[full]>=0.44.4" },
-    expected: { version: "[full]>=0.44.4", python: "python3" },
+    expected: {
+      version: "[full]>=0.44.4",
+      python: "python3",
+      install: "cfn-lint[full]>=0.44.4",
+    },
   },
   {
     platform: "linux",
     input: { python: "python3" },
-    expected: { version: "[full]==1.*", python: "python3" },
+    expected: {
+      version: "[full]==1.*",
+      python: "python3",
+      install: "cfn-lint[full]==1.*",
+    },
   },
   {
     platform: "linux",
     input: { version: "[full]~=0.44.4", python: "python3" },
-    expected: { version: "[full]~=0.44.4", python: "python3" },
+    expected: {
+      version: "[full]~=0.44.4",
+      python: "python3",
+      install: "cfn-lint[full]~=0.44.4",
+    },
+  },
+  {
+    platform: "linux",
+    input: { version: "1.2.3", python: "python3" },
+    expected: {
+      version: "1.2.3",
+      python: "python3",
+      install: "cfn-lint[full]==1.2.3",
+    },
   },
 ])("setup %o", async (test) => {
   jest.spyOn(os, "platform").mockReturnValue(test.platform);
@@ -58,7 +91,7 @@ test.each([
   expect(io.which).toHaveBeenCalledWith(test.expected.python, true);
   expect(exec.exec).toHaveBeenCalledWith(
     expect.anything(),
-    expect.arrayContaining(["install", `cfn-lint${test.expected.version}`]),
+    expect.arrayContaining(["install", test.expected.install]),
   );
   expect(core.addPath).toHaveBeenCalledTimes(1);
 });
